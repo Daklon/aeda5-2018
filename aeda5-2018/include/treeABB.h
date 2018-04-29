1e889b4c9eadb714b0c962dae6fe08cbf840e4ab
Javier Herrera <muchorrollodejavi@hotmail.com>
2018-04-29 17:32:39 +0100

    added statistics to treeABB, added user input(not finished),fixed small bugs in treeABB Buscar method

--- aeda5-2018/include/treeABB.h
+++ aeda5-2018/include/treeABB.h
@@ -10,17 +10,22 @@ class Treeabb
 public:
     Treeabb(){
         this->numOfElements = 0;
+        this->numOfComparisons = 0;
         this->head = nullptr;
+        this->temp_data = nullptr;
+        this->temp_head = nullptr;
     }
 
-    NodeBB* Buscar(Clave x){
+    NodeBB<Clave>* Buscar(Clave x){
         this->temp_head = this->head;
         Clave temp_clave;
         bool itera = true;
-        NodeBB *encontrado = nullptr;
+        NodeBB<Clave> *encontrado = nullptr;
         //iteramos
         while(itera){
             temp_clave = this->temp_head->GetClave();
+            //incrementamos las comparaciones
+            this->numOfComparisons++;
             //si el nodo "raiz" es la clave, la devolvemos
             if(x == temp_clave){
                 itera = false;
@@ -51,33 +56,44 @@ public:
     }
 
     void Insertar(Clave x){
+        //si el arbol esta vacio
         if(this->numOfElements == 0){
             this->head = new NodeBB<Clave>(x);
             this->numOfElements++;
+        //si no esta vacio
         } else {
             bool itera = true;
             this->temp_head = this->head;
             this->temp_data = new NodeBB<Clave>(x);
             while (itera) {
+                //aumentamos el contador
+                this->numOfComparisons++;
+                //subarbol derecho
                 if (x > this->temp_head->GetClave()){
+                    //si es null, insertamos
                     if(this->temp_head->GetRightChildren() == nullptr){
                         this->temp_head->SetRightChildren(this->temp_data);
                         this->temp_data->SetFather(this->temp_head);
                         this->numOfElements++;
                         itera = false;
+                    //si no es null, entramos e iteramos
                     } else {
                         this->temp_head = this->temp_head->GetRightChildren();
                     }
+                //subarbol izquierdo
                 } else if(x < this->temp_head->GetClave()){
+                    //si es null entramos
                     if(this->temp_head->GetLeftChildren() == nullptr){
                         this->temp_head->SetLeftChildren(this->temp_data);
                         this->temp_data->SetFather(this->temp_head);
                         this->numOfElements++;
                         itera = false;
+                    //si no es null entramos e iteramos
                     } else {
                         this->temp_head = this->temp_head->GetLeftChildren();
                     }
                 } else {
+                    //si estamos insertando una copia, aumentamos el contador de copias
                     this->temp_head->IncreaseCounter();
                 }
             }
@@ -97,6 +113,7 @@ public:
                 //si no, el nodo a borrar es el izquierdo de su padre, lo borro
                 this->temp_head->GetFather()->SetLeftChildren(nullptr);
                 delete this->temp_head;
+                this->numOfElements;
             }
             //si el nodo tiene dos hijos
         } else if(this->temp_head->GetLeftChildren() != nullptr && this->temp_head->GetRightChildren() != nullptr){
@@ -112,6 +129,7 @@ public:
                     this->temp_head->GetFather()->SetRightChildren(this->temp_head->GetLeftChildren());
                 }
                 delete this->temp_head;
+                this->numOfElements--;
             } else {
                 //si no, el nodo a borrar es el izquierdo de su padre, lo borro
                 if(temp_head->GetRightChildren() != nullptr){
@@ -120,6 +138,7 @@ public:
                     this->temp_head->GetFather()->SetLeftChildren(this->temp_head->GetLeftChildren());
                 }
                 delete this->temp_head;
+                this->numOfElements--;
             }
         }
     }
@@ -127,12 +146,19 @@ public:
     void print(){
 
     }
-    
+
+    int GetComparisons(){
+        return this->numOfComparisons;
+    }
+    void ResetStatistics(){
+        this->numOfComparisons=0;
+    }
 private:
     NodeBB<Clave> *head;
     NodeBB<Clave> *temp_head;
     NodeBB<Clave> *temp_data;
     int numOfElements;
+    int numOfComparisons;
 };
 
 
--- aeda5-2018/src/main.cpp
+++ aeda5-2018/src/main.cpp
@@ -1,19 +1,62 @@
 #include <iostream>
 #include "treeABB.h"
+#include <vector>
 
 using namespace std;
 
+int tamArbol,nPruebas,mode;
+
+void AskData(){
+    cout << "Introduzca el tamaño del arbol:";
+    cin >> tamArbol;
+    cout << "Introduzca el número de pruebas:";
+    cin >> nPruebas;
+}
+
+void SelectMode(){
+    cout << "--------------------------------" << endl;
+    cout << "    Arbol de busqueda binario"<< endl<<endl;
+    cout << "Elija el modo:" <<endl<< "0 - Demostracion"<<endl<<"1 - Estadistica"<<endl;
+    cin >> mode;
+}
+
 int main()
 {
-    Treeabb<int> arbol;
-    arbol.Insertar(8);
-    arbol.Insertar(3);
-    arbol.Insertar(10);
-    arbol.Insertar(1);
-    arbol.Insertar(6);
-    arbol.Insertar(14);
-    arbol.Insertar(4);
-    arbol.Insertar(7);
-    arbol.Insertar(13);
+    if(mode == 0){
+        //demo
+    } else if(mode==1){
+        //estadistica
+        AskData();
+        //creamos el banco de pruebas
+        vector<DNI> testbench;
+        for(int i=0;i<2*N;i++){
+            DNI aleat;
+            testbench.push_back(aleat);
+        }
+        //creamos el arbol y insertamos la mitad de elementos del banco de pruebas
+        Treeabb<DNI> arbol;
+        for(int i=0;i<N;i++){
+            arbol.Insertar(testbench.at(i));
+        }
+
+        //reseteamos estadisitcas
+        arbol.ResetStatistics();
+
+
+    } else if(mode == 2){
+        Treeabb<int> arbol;
+        arbol.Insertar(8);
+        arbol.Insertar(3);
+        arbol.Insertar(10);
+        arbol.Insertar(1);
+        arbol.Insertar(6);
+        arbol.Insertar(14);
+        arbol.Insertar(4);
+        arbol.Insertar(7);
+        arbol.Insertar(13);
+    } else {
+        cout << "Modo incorrecto";
+        return 0;
+    }
     return 0;
 }
